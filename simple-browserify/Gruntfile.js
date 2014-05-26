1c0a735d7e7ed50debafd1e9ec4b7a8c8531baaf
Nico Rehwaldt <git_nikku@nixis.de>
2014-05-26 09:36:23 +0200

    chore(simple-browserify): initial commit

---
+++ simple-browserify/Gruntfile.js
@@ -0,0 +1,98 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+
+  // project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    config: {
+      sources: 'app',
+      dist: 'dist'
+    },
+
+    jshint: {
+      src: [
+        ['<%=config.sources %>']
+      ],
+      options: {
+        jshintrc: true
+      }
+    },
+
+    browserify: {
+      options: {
+        transform: [ 'brfs' ],
+        browserifyOptions: {
+          builtins: [ 'fs' ],
+          commondir: false
+        },
+        bundleOptions: {
+          detectGlobals: false,
+          insertGlobalVars: [],
+          debug: true
+        }
+      },
+      watch: {
+        options: {
+          watch: true,
+          keepalive: true
+        },
+        files: {
+          '<%= config.dist %>/app.js': [ '<%= config.sources %>/**/*.js' ]
+        }
+      },
+      app: {
+        files: {
+          '<%= config.dist %>/app.js': [ '<%= config.sources %>/**/*.js' ]
+        }
+      }
+    },
+    copy: {
+      app: {
+        files: [
+          {
+            expand: true,
+            cwd: '<%= config.sources %>/',
+            src: ['**/*.*', '!**/*.js'],
+            dest: '<%= config.dist %>'
+          }
+        ]
+      }
+    },
+    watch: {
+      samples: {
+        files: [ '<%= config.sources %>/**/*.*' ],
+        tasks: [ 'copy:app' ]
+      },
+    },
+    connect: {
+      options: {
+        port: 9013,
+        livereload: 9014,
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= config.dist %>'
+          ]
+        }
+      }
+    }
+  });
+
+  // tasks
+
+  grunt.registerTask('build', [ 'browserify:app', 'copy:app' ]);
+
+  grunt.registerTask('auto-build', [
+    'browserify:watch',
+    'connect:livereload',
+    'watch'
+  ]);
+
+  grunt.registerTask('default', [ 'jshint', 'build' ]);
+};
---
+++ simple-browserify/app/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// we use fs + brfs to inline an example XML document.
+// exclude fs in package.json#browser + use the brfs transform
+// to generate a clean browserified bundle
+var fs = require('fs');
+
+// inlined in result file via brfs
+var pizzaDiagram = fs.readFileSync(__dirname + '/../resources/pizza-collaboration.bpmn', 'utf-8');
+
+
+
+// require the viewer, make sure you added it to your project
+// dependencies via npm install --save-dev bpmn-js
+var BpmnViewer = require('bpmn-js/lib/Viewer');
+
+
+var viewer = new BpmnViewer({ container: '#canvas' });
+
+viewer.importXML(pizzaDiagram, function(err) {
+
+  if (!err) {
+    console.log('success!');
+    viewer.get('canvas').zoom('fit-viewport');
+  } else {
+    console.log('something went wrong:', err);
+  }
+});
