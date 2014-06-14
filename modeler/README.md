b0832b7b64933df2e126a542c16840d281ddfcc3
Nico Rehwaldt <git_nikku@nixis.de>
2014-06-14 14:12:23 +0200

    feat(modeler): add modeler example
    This commit restores the modeler example that has previously been
    removed from bpmn-io/bpmn-js.

---
+++ modeler/README.md
@@ -0,0 +1,36 @@
+# bpmn-js Modeler Example
+
+This example uses [bpmn-js](https://github.com/bpmn-io/bpmn-js) to implement a modeler for BPMN 2.0 process diagrams. It serves as the basis of the bpmn-js demo application available at [demo.bpmn.io](http://demo.bpmn.io).
+
+## About
+
+This example is a node-style web application that builds a user interface around the bpmn-js BPMN 2.0 modeler.
+
+![demo application screenshot](https://raw.githubusercontent.com/bpmn-io/bpmn-js-examples/master/modeler/docs/screenshot.png "Screenshot of the example application")
+
+
+## Building
+
+You need a [NodeJS](http://nodejs.org) development stack with [npm](https://npmjs.org) and [grunt](http://gruntjs.com) installed to build the project.
+
+To install all project dependencies execute
+
+```
+npm install
+```
+
+Build the application (including [bpmn-js](https://github.com/bpmn-io/bpmn-js)) using [browserify](http://browserify.org) via
+
+```
+grunt
+```
+
+You may also spawn a development setup by executing
+
+```
+grunt auto-build
+```
+
+Both tasks generate the distribution ready client-side modeler application into the `dist` folder.
+
+Serve the application locally or via a web server (nginx, apache, embedded).
---
+++ modeler/app/index.js
@@ -0,0 +1,144 @@
+var fs = require('fs');
+
+var $ = require('jquery'),
+    BpmnModeler = require('bpmn-js/lib/Modeler');
+
+var container = $('#js-drop-zone');
+
+var canvas = $('#js-canvas');
+
+var renderer = new BpmnModeler({ container: canvas, width: '100%', height: '100%' });
+
+var newDiagramXML = fs.readFileSync(__dirname + '/../resources/newDiagram.bpmn', 'utf-8');
+
+function createNewDiagram() {
+  openDiagram(newDiagramXML);
+}
+
+function openDiagram(xml) {
+
+  renderer.importXML(xml, function(err) {
+
+    if (err) {
+      container
+        .removeClass('with-diagram')
+        .addClass('with-error');
+
+      container.find('.error pre').text(err.message);
+
+      console.error(err);
+    } else {
+      container
+        .removeClass('with-error')
+        .addClass('with-diagram');
+    }
+
+
+  });
+}
+
+function saveSVG(done) {
+  renderer.saveSVG(done);
+}
+
+function saveDiagram(done) {
+
+  renderer.saveXML({ format: true }, function(err, xml) {
+    done(err, xml);
+  });
+}
+
+function registerFileDrop(container, callback) {
+
+  function handleFileSelect(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var files = e.dataTransfer.files;
+
+    var file = files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+
+      var xml = e.target.result;
+
+      callback(xml);
+    };
+
+    reader.readAsText(file);
+  }
+
+  function handleDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  container.get(0).addEventListener('dragover', handleDragOver, false);
+  container.get(0).addEventListener('drop', handleFileSelect, false);
+}
+
+
+////// file drag / drop ///////////////////////
+
+// check file api availability
+if (!window.FileList || !window.FileReader) {
+  window.alert(
+    'Looks like you use an older browser that does not support drag and drop. ' +
+    'Try using Chrome, Firefox or the Internet Explorer > 10.');
+} else {
+  registerFileDrop(container, openDiagram);
+}
+
+// bootstrap diagram functions
+
+$(document).on('ready', function() {
+
+  $('#js-create-diagram').click(function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    createNewDiagram();
+  });
+
+  var downloadLink = $('#js-download-diagram');
+  var downloadSvgLink = $('#js-download-svg');
+
+  $('.buttons a').click(function(e) {
+    if (!$(this).is('.active')) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+
+  function setEncoded(link, name, data) {
+    var encodedData = encodeURIComponent(data);
+
+    if (data) {
+      link.addClass('active').attr({
+        'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
+        'download': name
+      });
+    } else {
+      link.removeClass('active');
+    }
+  }
+
+  var _ = require('lodash');
+
+  var exportArtifacts = _.debounce(function() {
+
+    saveSVG(function(err, svg) {
+      setEncoded(downloadSvgLink, 'diagram.svg', err ? null : svg);
+    });
+
+    saveDiagram(function(err, xml) {
+      setEncoded(downloadLink, 'diagram.bpmn', err ? null : xml);
+    });
+  }, 500);
+
+  renderer.on('commandStack.changed', exportArtifacts);
+});
