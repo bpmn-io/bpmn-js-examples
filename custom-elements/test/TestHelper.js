e64db2c135c350ad070ed69ca38c45e486d692e5
Nico Rehwaldt <git_nikku@nixis.de>
2018-02-22 10:28:30 +0100

    fix(custom-elements): correctly embed SVG images

--- custom-elements/test/TestHelper.js
+++ custom-elements/test/TestHelper.js
@@ -3,7 +3,7 @@
 var TestHelper = module.exports = require('bpmn-js/test/helper');
 
 TestHelper.insertCSS('diagram-js.css', require('diagram-js/assets/diagram-js.css'));
-TestHelper.insertCSS('bpmn-embedded.css', require('bpmn-js/assets/bpmn-font/css/bpmn-embedded.css'));
+TestHelper.insertCSS('bpmn-embedded.css', require('bpmn-font/dist/css/bpmn-embedded.css'));
 
 TestHelper.insertCSS('diagram-js-testing.css',
   '.test-container .result { height: 500px; }' + '.test-container > div'
@@ -11,9 +11,9 @@ TestHelper.insertCSS('diagram-js-testing.css',
 
 TestHelper.insertCSS('custom-modeler-testing.css',
   '.icon-custom-triangle {'
-    + 'background: url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="#3CAA82" width="270" height="240"><path d="M8,40 l 15,-27 l 15,27 z"/></svg>\');'
+    + 'background: url(\'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20fill%3D%22%233CAA82%22%20width%3D%22270%22%20height%3D%22240%22%3E%3Cpath%20d%3D%22M8%2C40%20l%2015%2C-27%20l%2015%2C27%20z%22%2F%3E%3C%2Fsvg%3E\');'
   + '}'
   + '.icon-custom-circle {'
-    + 'background: url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" stroke-width="8" stroke="#48a" fill="none" viewBox="0 0 120 120"><circle cx="60" cy="60" r="40"/></svg>\');'
+    + 'background: url(\'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20stroke-width%3D%228%22%20stroke%3D%22%2348a%22%20fill%3D%22none%22%20viewBox%3D%220%200%20120%20120%22%3E%3Ccircle%20cx%3D%2260%22%20cy%3D%2260%22%20r%3D%2240%22%2F%3E%3C%2Fsvg%3E\');'
   + '}'
 );
