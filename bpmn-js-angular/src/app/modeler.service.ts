326ccb1b55614ba1ce4974453a9a93f072ac57e1
Daniel Kreiger <d.kreiger@compuccino.com>
2018-12-05 13:57:56 +0100

    chore(http): better error handling

--- bpmn-js-angular/src/app/modeler.service.ts
+++ bpmn-js-angular/src/app/modeler.service.ts
@@ -1,23 +1,35 @@
-import { HttpClient } from '@angular/common/http';
-import BpmnJS from 'bpmn-js/dist/bpmn-modeler.development.js';
-import { map } from 'rxjs/operators';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import BpmnJS from "bpmn-js/dist/bpmn-modeler.development.js";
+import { map, catchError, retry } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
 
 @Injectable()
 export class ModelerService {
   constructor(private http: HttpClient) {}
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error("An error occurred:", error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError("Something bad happened; please try again later.");
+  }
+
   fetchXML(viewer: BpmnJS, xmlSrc: string) {
-    return this.http.get(xmlSrc, { responseType: 'text' }).pipe(
-      map(res => {
-        viewer.importXML(res, err => {
-          if (err) {
-            console.log('error rendering', err);
-          } else {
-            console.log('rendered');
-          }
-        });
-      })
+    return this.http.get(xmlSrc, { responseType: "text" }).pipe(
+      map((res: string) => {
+        viewer.importXML(res);
+      }),
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError)
     );
   }
 }
