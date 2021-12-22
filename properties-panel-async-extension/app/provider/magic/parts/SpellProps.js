b8428d87e3d91d97e02558202b1101503a6a352e
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-12-22 17:40:34 +0100

    feat: add async example
    Closes #148

---
+++ properties-panel-async-extension/app/provider/magic/parts/SpellProps.js
@@ -0,0 +1,68 @@
+import { SelectEntry, isSelectEntryEdited } from '@bpmn-io/properties-panel';
+import { useService } from 'bpmn-js-properties-panel'
+
+import { useEffect, useState } from '@bpmn-io/properties-panel/preact/hooks';
+
+export default function(element) {
+
+  return [
+    {
+      id: 'spell',
+      component: <Spell id="spell" element={ element } />,
+      isEdited: isSelectEntryEdited
+    }
+  ];
+}
+
+function Spell(props) {
+  const { element, id } = props;
+
+  const modeling = useService('modeling');
+  const translate = useService('translate');
+  const debounce = useService('debounceInput');
+
+
+  const getValue = () => {
+    return element.businessObject.spell || '';
+  }
+
+  const setValue = value => {
+    return modeling.updateProperties(element, {
+      spell: value
+    });
+  }
+
+  const [ spells, setSpells ] = useState([]);
+
+  useEffect(() => {
+    function fetchSpells() {
+      fetch('http://localhost:1234/spell')
+        .then(res => res.json())
+        .then(spellbook => setSpells(spellbook))
+        .catch(error => console.error(error));
+    }
+
+    fetchSpells();
+  }, [ setSpells ]);
+
+  const getOptions = () => {
+    return [
+      { label: '<none>', value: undefined },
+      ...spells.map(spell => ({
+        label: spell,
+        value: spell
+      }))
+    ];
+  }
+
+  return <SelectEntry
+    id={ id }
+    element={ element }
+    description={ translate('Apply a black magic spell') }
+    label={ translate('Spell') }
+    getValue={ getValue }
+    setValue={ setValue }
+    getOptions={ getOptions }
+    debounce={ debounce }
+  />
+}
