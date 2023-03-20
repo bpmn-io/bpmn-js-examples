d591d39620d202c51fa7774b4ae0c920fe03822d
Beatriz Mendes <bea.smendes98@gmail.com>
2023-03-20 10:51:47 +0100

    deps(theming): update to rollup v3
    - Update rollup and rollup plugins
    - Move to @rollup/* bundling plug-ins

--- theming/rollup.config.js
+++ theming/rollup.config.js
@@ -1,6 +1,6 @@
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/custom-viewer.js',
