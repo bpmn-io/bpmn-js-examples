89e7cf0ae84b37f06ec9a7ca08d2bd0241187ddc
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-01-20 08:50:10 +0100

    feat(embedding): feature shared clipboard
    Allows to try out keyboard shortcuts in a more advanced scenario.

--- embedding/src/app.js
+++ embedding/src/app.js
@@ -9,27 +9,52 @@ import BpmnModeler from 'bpmn-js/lib/Modeler';
 import BpmnViewer from 'bpmn-js/lib/Viewer';
 import BpmnNavigatedViewer from 'bpmn-js/lib/NavigatedViewer';
 
+import Clipboard from 'diagram-js/lib/features/clipboard/Clipboard';
+
 const editorMap = {
   'editor': BpmnModeler,
   'viewer': BpmnViewer,
-  'navigated-viewer': BpmnNavigatedViewer,
-  'default': BpmnModeler
+  'navigated-viewer': BpmnNavigatedViewer
+};
+
+const sharedClipboard = {
+  'clipboard': [ 'value', new Clipboard() ]
+};
+
+const additionalModulesMap = {
+  'first-modeler': [
+    sharedClipboard
+  ],
+  'second-modeler': [
+    sharedClipboard
+  ]
 };
 
 const modelers = new Map();
+
+function createModeler(viewerType, config, id) {
+
+  const BpmnJS = editorMap[viewerType] || BpmnModeler;
+
+  const additionalModules = additionalModulesMap[id] || [];
+
+  return new BpmnJS({
+    ...config,
+    additionalModules
+  });
+}
 
 async function openDiagram(element) {
 
   const diagramURL = element.dataset.diagram;
   const viewerType = element.dataset.editor;
+  const id = element.id || '__default';
 
-  const BpmnJS = editorMap[viewerType] || editorMap.default;
-
-  const diagramXML = await fetch(diagramURL).then(response => response.text());
-
-  const modeler = new BpmnJS({ container: element });
+  const modeler = createModeler(viewerType, { container: element }, id);
 
   modelers.set(element, modeler);
+
+  const diagramXML = await fetch(diagramURL).then(response => response.text());
 
   await modeler.importXML(diagramXML);
 }
